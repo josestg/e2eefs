ea104705b1c4c3ecaabe9ba7227483eb90a8b10b
Jose Sitanggang <josealfredositanggang@gmail.com>
2025-10-19 21:12:50 +0700

    L002: done

--- cmd/lattice/lattice.go
+++ cmd/lattice/lattice.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Lattice")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("PONG!"))
+		if err != nil {
+			log.Printf("cannot reply: %s", err.Error())
+		}
+	})
+
+	srv := http.Server{
+		Addr:    "localhost:8080", // host:port
+		Handler: mux,
+	}
+
+	log.Printf("server is listening: %s", srv.Addr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Println("error:", err.Error())
+		os.Exit(1)
+	}
 }
